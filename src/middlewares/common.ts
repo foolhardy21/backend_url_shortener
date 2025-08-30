66ae50a589c04737569aafbe6abc05833486688b
Vinay Kumar <vinay@Vinsys-MacBook-Pro.local>
2025-08-30 19:29:42 +0530

    feat: common api-key validator

---
+++ src/middlewares/common.ts
@@ -0,0 +1,46 @@
+import { NextFunction, Response, Request } from "express"
+import logModel from "../models/logModel"
+import userModel from "../models/userModel"
+
+export async function logger(req: Request, _: Response, next: NextFunction) {
+    try {
+        const timestamp = new Date()
+        const url = req.originalUrl
+        const method = req.method
+        const userAgent = req.headers["user-agent"]
+        const ipAddress = req.ip
+
+        console.log(method + " " + url, " at ", timestamp, " from ", userAgent, " | ", ipAddress)
+
+        await logModel.createLog({
+            url,
+            method,
+            timestamp,
+            userAgent,
+            ipAddress,
+        })
+    } catch (err) {
+        console.log(err)
+    } finally {
+        next()
+    }
+}
+
+export async function apiKeyValidator(req: Request, res: Response, next: NextFunction) {
+    try {
+        const requestApiKey = req.headers["x-api-key"]
+        if (!requestApiKey) {
+            return res.status(401).json({ success: false, message: "Missing API key." })
+        }
+        const userRes = await userModel.getUserByApiKey(requestApiKey as string)
+        if (userRes.length === 0) {
+            return res.status(401).json({ success: false, message: "Invalid API key." })
+        }
+        (req as any).tier = userRes[0].tier;
+        (req as any).userId = userRes[0].id
+        next()
+    } catch (err) {
+        console.log(err)
+        return res.status(401).json({ success: false, message: "Could not validate the API key." })
+    }
+}
--- src/routes/urlRoutes.ts
+++ src/routes/urlRoutes.ts
@@ -2,17 +2,17 @@ import { Router } from "express"
 import multer from "multer"
 import urlController from "../controllers/urlController"
 import validators from "../middlewares/validators"
-import { Logger } from "../middlewares/logger"
+import { apiKeyValidator, logger } from "../middlewares/common"
 
 const router = Router()
 
 const fileUpload = multer({ dest: "uploads/" })
 
-router.post("/shorten", Logger, validators.validateShortenPayload, urlController.createShortUrl)
-router.patch("/redirect", Logger, validators.validateRedirectQuery, urlController.updateUrlMetaData, urlController.getOriginalUrl)
-router.delete("/", Logger, validators.validateDeleteParams, urlController.deleteByOriginalUrl)
-router.post("/bulk-shorten", Logger, fileUpload.single("urlsCsv"), validators.validateBulkShortenPayload, urlController.createBulkShortUrls)
-router.patch("/update", Logger, validators.validateUpdateQuery, urlController.updateUrlExpiry)
-router.get("/user/:userId", Logger, validators.validateGetUserUrls, urlController.getUserUrls)
+router.post("/shorten", logger, apiKeyValidator, validators.validateShortenPayload, urlController.createShortUrl)
+router.patch("/redirect", logger, validators.validateRedirectQuery, urlController.updateUrlMetaData, urlController.getOriginalUrl)
+router.delete("/", logger, apiKeyValidator, validators.validateDeleteParams, urlController.deleteByOriginalUrl)
+router.post("/bulk-shorten", logger, apiKeyValidator, fileUpload.single("urlsCsv"), validators.validateBulkShortenPayload, urlController.createBulkShortUrls)
+router.patch("/update", logger, validators.validateUpdateQuery, urlController.updateUrlExpiry)
+router.get("/user/:userId", logger, validators.validateGetUserUrls, urlController.getUserUrls)
 
 export default router
